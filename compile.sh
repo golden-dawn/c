3e8f464aa5e2996e3eba9f89191239451f4bcb71
golden-dawn <constantin_m_adam@yahoo.com>
2019-04-26 02:36:49 -0400

    Explicit include flags; remove c99 flag

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 export SRC_FILE=$1
 export EXE_FILE="${SRC_FILE/\.c/\.exe}"
-gcc -o ${EXE_FILE} ${SRC_FILE} -I/usr/include/postgresql -lpq -std=c99
+gcc -o ${EXE_FILE} ${SRC_FILE} -I. -I/usr/include/postgresql -lpq
