f9158be03e37a5e448c69a39858fad65d3ea392c
golden-dawn <constantin_m_adam@yahoo.com>
2019-04-27 03:30:57 -0400

    WIP: added split retrieval; find record based on index

--- stx_ts.h
+++ stx_ts.h
@@ -34,7 +34,6 @@ float wprice( daily_record_ptr data, int ix) {
 daily_record_ptr load_stk(char* stk, int* num_recs) {
 
     daily_record_ptr result = NULL;
-    /* daily_record_ptr cursor = NULL; */
     char sql_cmd[80];
     sprintf(sql_cmd, "select o, hi, lo, c, v, dt from eods where stk='%s' "
 	    "order by dt", stk);
@@ -54,37 +53,50 @@ daily_record_ptr load_stk(char* stk, int* num_recs) {
     }
     PQclear(res);
     return result;
-    /* fp= fopen( filename, "r"); */
-    /* memset( line, 0, 80); */
-    /* cursor= result; */
-    /* while( fgets( line, 80, fp)) */
-    /* 	read_file_line( line, cursor++); */
-    /* fclose( fp); */
-    /* return result; */
 }
 
-/* int find_date_record( daily_record_ptr data, int lines, char* date) { */
+divi_ptr load_splits(char* stk, int* num_recs) {
+    divi_ptr result = NULL;
+    char sql_cmd[80];
+    sprintf(sql_cmd, "select ratio, dt from dividends where stk='%s' "
+	    "order by dt", stk);
+    PGresult *res = db_query(sql_cmd);
+    if((*num_recs = PQntuples(res)) <= 0) 
+	return result;
+    int num = *num_recs;
+    result = (daily_record_ptr) malloc(num * sizeof(daily_record));
+    memset(result, 0, num * sizeof(daily_record));
+    for(int ix = 0; ix < num; ix++) {
+	result[ix].ratio = atof(PQgetvalue(res, ix, 0));
+	strcpy(result[ix].date, PQgetvalue(res, ix, 1));
+    }
+    PQclear(res);
+    return result;
 
-/*     time_t date_time; */
-/*     time_t crs_time; */
-/*     int    i= 0, j= lines- 1, mid= ( i+ j)/ 2; */
+}
 
-/*     if( !strcmp( data[ j].date, date)) */
-/* 	return j; */
+int find_date_record(daily_record_ptr data, int lines, char* date) {
 
-/*     date_time= get_time_from_date( date); */
-/*     while( i<= j) { */
-/* 	crs_time= get_time_from_date( data[ mid].date); */
-/* 	if( crs_time< date_time) */
-/* 	    i= mid+ 1; */
-/* 	else if( crs_time> date_time) */
-/* 	    j= mid- 1; */
-/* 	else */
-/* 	    return mid; */
-/* 	mid= ( i+ j)/ 2; */
-/*     } */
-/*     return -1; */
-/* } */
+    time_t date_time;
+    time_t crs_time;
+    int i = 0, j = lines- 1, mid = (i + j) / 2;
+
+    if( !strcmp( data[ j].date, date))
+	return j;
+
+    date_time= get_time_from_date(date);
+    while( i<= j) {
+	crs_time= get_time_from_date( data[ mid].date);
+	if( crs_time< date_time)
+	    i= mid+ 1;
+	else if( crs_time> date_time)
+	    j= mid- 1;
+	else
+	    return mid;
+	mid= ( i+ j)/ 2;
+    }
+    return -1;
+}
 
 /* data_fragment_ptr get_fragment( daily_record_ptr data, int nb_lines, */
 /* 				time_frame_ptr tf) { */
