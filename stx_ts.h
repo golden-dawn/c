e787cc08c7a6b64e8fc63af5bf8d8f4fcde0dd29
golden-dawn <constantin_m_adam@yahoo.com>
2019-05-18 11:30:01 -0400

    Load data for all dates in the interval between start and end date

--- stx_ts.h
+++ stx_ts.h
@@ -6,8 +6,7 @@
 #include <string.h>
 #include <libpq-fe.h>
 #include <time.h>
-#include "stx_db.h"
-#include "stx_ht.h"
+#include "stx_core.h"
 
 /** BEGIN: macros */
 #define module( x) (( x>  0)? x: -x)
@@ -28,24 +27,8 @@ typedef struct stx_data_t {
     daily_record_ptr data;
     int num_recs;
     hashtable_ptr splits;
+    int pos;
 } stx_data, *stx_data_ptr;
-
-
-void print_timestamp() {
-    long milliseconds;
-    time_t seconds;
-    struct timespec spec;
-    char buff[20];
-    clock_gettime(CLOCK_REALTIME, &spec);
-    seconds = spec.tv_sec;
-    milliseconds = round(spec.tv_nsec / 1.0e6);
-    if (milliseconds > 999) {
-	seconds++;
-	milliseconds = 0;
-    }
-    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&seconds));
-    fprintf(stderr, "%s.%ld [] ", buff, milliseconds);
-}
 
 
 float wprice( daily_record_ptr data, int ix) {
@@ -59,11 +42,11 @@ hashtable_ptr load_splits(char* stk) {
 	    "order by dt", stk);
     PGresult *res = db_query(sql_cmd);
 #ifdef DEBUG
-    fprintf(stderr, "Found %d splits for %s\n", PQntuples(res), stk);
+    LOGDEBUG("Found %d splits for %s\n", PQntuples(res), stk);
 #endif
     hashtable_ptr result = ht_divis(res);
 #ifdef DEBUG
-    fprintf(stderr, "Loaded splits for %s\n", stk);
+    LOGDEBUG("Loaded splits for %s\n", stk);
 #endif
     PQclear(res);
     return result;
@@ -72,44 +55,108 @@ hashtable_ptr load_splits(char* stk) {
 
 stx_data_ptr load_stk(char* stk) {
 #ifdef DEBUG
-    fprintf(stderr, "Loading data for %s\n", stk);
+    LOGDEBUG("Loading data for %s\n", stk);
 #endif
     stx_data_ptr data = (stx_data_ptr) malloc(sizeof(stx_data));
     data->data = NULL;
     data->num_recs = 0;
-    char sql_cmd[80];
+    data->pos = 0;
+    char sql_cmd[128];
     sprintf(sql_cmd, "select o, hi, lo, c, v, dt from eods where stk='%s' "
-	    "order by dt", stk);
+	    "and dt>'1985-01-01'order by dt", stk);
     PGresult *res = db_query(sql_cmd);
     if((data->num_recs = PQntuples(res)) <= 0) 
 	return data;
     int num = data->num_recs;
+    char sd[16], ed[16];
+    strcpy(sd, PQgetvalue(res, 0, 5));
+    strcpy(ed, PQgetvalue(res, num - 1, 5));
+    int b_days = cal_num_busdays(sd, ed);
 #ifdef DEBUG
-    fprintf(stderr, "Found %d records for %s\n", num, stk);
+    LOGDEBUG("Found %d records for %s\n", num, stk);
 #endif
-    data->data = (daily_record_ptr) calloc(num, sizeof(daily_record));
+    data->data = (daily_record_ptr) calloc(b_days, sizeof(daily_record));
+    int ts_idx = 0;
+    int calix = cal_ix(sd);
+    char* dt;
+    calix = cal_prev_bday(calix, &dt);
     for(int ix = 0; ix < num; ix++) {
-	data->data[ix].open = atoi(PQgetvalue(res, ix, 0));
-	data->data[ix].high = atoi(PQgetvalue(res, ix, 1));
-	data->data[ix].low = atoi(PQgetvalue(res, ix, 2));
-	data->data[ix].close = atoi(PQgetvalue(res, ix, 3));
-	data->data[ix].volume = atoi(PQgetvalue(res, ix, 4));
-	strcpy(data->data[ix].date, PQgetvalue(res, ix, 5)); 
+	calix = cal_next_bday(calix, &dt);
+	char* db_date = PQgetvalue(res, ix, 5);
+	if (strcmp(dt, db_date) > 0) {
+	    LOGERROR("Something is very wrong: dt = %s, db_date = %s\n",
+		     dt, db_date);
+	    exit(-1);
+	}
+	while (strcmp(dt, db_date) < 0) {
+#ifdef DEBUG
+	    LOGDEBUG("Adding data for %s, not found in %s data\n", dt, stk);
+#endif
+	    data->data[ts_idx].open = data->data[ts_idx - 1].close;
+	    data->data[ts_idx].high = data->data[ts_idx - 1].close;
+	    data->data[ts_idx].low = data->data[ts_idx - 1].close;
+	    data->data[ts_idx].close = data->data[ts_idx - 1].close;
+	    data->data[ts_idx].volume = 0;
+	    strcpy(data->data[ts_idx].date, dt); 
+	    calix = cal_next_bday(calix, &dt);
+	    ts_idx++;
+	}   
+	data->data[ts_idx].open = atoi(PQgetvalue(res, ix, 0));
+	data->data[ts_idx].high = atoi(PQgetvalue(res, ix, 1));
+	data->data[ts_idx].low = atoi(PQgetvalue(res, ix, 2));
+	data->data[ts_idx].close = atoi(PQgetvalue(res, ix, 3));
+	data->data[ts_idx].volume = atoi(PQgetvalue(res, ix, 4));
+	strcpy(data->data[ts_idx].date, PQgetvalue(res, ix, 5)); 
+	ts_idx++;
     }
+    data->pos = b_days - 1;
     PQclear(res);
 #ifdef DEBUG
-    fprintf(stderr, "Loading the splits for %s\n", stk);
+    LOGDEBUG("Loading the splits for %s\n", stk);
 #endif
     data->splits = load_splits(stk);
 #ifdef DEBUG
-    fprintf(stderr, "Done loading %s\n", stk);
+    LOGDEBUG("Done loading %s\n", stk);
 #endif
     return data;
 }
 
 
-int find_date_record(stx_data_ptr data, char* date) {
+int find_date_record(stx_data_ptr data, char* date, int rel_pos) {
+    /** rel_pos is a parameter that can take the following values:
+     *  0 - do an exact search
+     *  1 - return date, or next business day, if date not found
+     * -1 - return date, or previous business day, if date not found
+     **/
     char* first_date = data->data[0].date;
+    int n = cal_num_busdays(first_date, date);
+/*     if (n < 0) { */
+/* 	if (rel_pos > 0) */
+/* 	    return 0; */
+/*     } else if (n >= data->num_recs) { */
+/* 	if (rel_pos < 0) */
+/* 	    return data->num_recs - 1;; */
+	
+/* 	return -1; */
+
+/*         n = stxcal.num_busdays(self.sd_str, dt) */
+/*         if n < 0: */
+/*             if c > 0: */
+/*                 return 0 */
+/*         elif n >= self.l: */
+/*             if c < 0: */
+/*                 return self.l - 1 */
+/*         else: */
+/*             df_dt = str(self.df.index[n].date()) */
+/*             if df_dt == dt: */
+/*                 return n */
+/*             # df_dt will always be less than dt, if dt is not a business day */
+/*             if c < 0: */
+/*                 return n - 1 */
+/*             if c > 0: */
+/*                 return n */
+/*         return -1 */
+    
     return 0;
 }
 /*     int num_days = stx_time_num_bus_days(first_date, date); */
