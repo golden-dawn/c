269bf85e3d35ad1925cdc8e28a27d2ae00e8f464
golden-dawn <constantin_m_adam@yahoo.com>
2019-06-07 02:50:20 -0400

    WIP: successfully compiled jl.exe

--- jl.c
+++ jl.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "stx_jl.h"
+#include "stx_ts.h"
 
 int main(int argc, char** argv) {
     char stk[16], ed[16];
@@ -14,26 +18,9 @@ int main(int argc, char** argv) {
 	    factor = atof(argv[ix]);
     }
     stx_data_ptr data = ts_load_stk(stk);
-    jl_data_ptr jl = jl_init20(data, factor);
-    
-    stk = sys.argv[1]
-    sd = sys.argv[2]
-    ed = sys.argv[3]
-    dt = sys.argv[4]
-    factor = float(sys.argv[5])
-    ts = StxTS(stk, sd, ed)
-    jl = StxJL(ts, factor)
-    jlres = jl.jl(dt)
-    jl.jl_print()
-    pivs = jl.get_pivots_in_days(100)
-    print("Pivs in 100 days:")
-    jl.print_pivs(pivs)
-    pivs = jl.get_num_pivots(4)
-    print("4 pivs:")
-    jl.print_pivs(pivs)
-    # jl.jl_print(print_pivots_only = True)
-    # pd.set_option('display.max_rows', 2000)
-    # pd.set_option('display.max_columns', 1500)
-    # pd.set_option('display.width', 1500)
-    # print(jlres)
-)
+    jl_data_ptr jl = jl_jl(data, "2019-05-24", factor);
+    jl_print(jl, false, false);
+/*     pivs = jl.get_num_pivots(4) */
+/*     print("4 pivs:") */
+/*     jl.print_pivs(pivs) */
+}
