e98157f24baec302176b7d694abbc5cfbac49ecd
golden-dawn <constantin_m_adam@yahoo.com>
2019-06-15 23:36:25 -0400

    Added configuration parameters to jl.c

--- jl.c
+++ jl.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,18 +10,26 @@ int main(int argc, char** argv) {
     strcpy(stk, "NFLX");
     strcpy(ed, "2019-05-17");
     float factor = 1.5;
+    int num_pivots = 4;
+    bool print_pivots_only = false, print_nils = false;
+
     for (int ix = 1; ix < argc; ix++) {
-	if (!strcmp(argv[ix], "-n") && (++ix < argc))
+	if (!strcmp(argv[ix], "-s") && (++ix < argc))
 	    strcpy(stk, argv[ix]);
 	else if (!strcmp(argv[ix], "-e") && (++ix < argc))
 	    strcpy(ed, argv[ix]);
 	else if (!strcmp(argv[ix], "-f") && (++ix < argc))
 	    factor = atof(argv[ix]);
+	else if (!strcmp(argv[ix], "-n") && (++ix < argc))
+	    num_pivots = atoi(argv[ix]);
+	else if (!strcmp(argv[ix], "-ppo"))
+	    print_pivots_only = true;
+	else if (!strcmp(argv[ix], "-pnils"))
+	    print_nils = true;
     }
     stx_data_ptr data = ts_load_stk(stk);
-    jl_data_ptr jl = jl_jl(data, "2019-05-17", factor);
-    jl_print(jl, false, false);
-/*     pivs = jl.get_num_pivots(4) */
-/*     print("4 pivs:") */
-/*     jl.print_pivs(pivs) */
+    jl_data_ptr jl = jl_jl(data, ed, factor);
+    jl_print(jl, print_pivots_only, print_nils);
+    int num;
+    jl_print_pivots(jl, num_pivots, &num);
 }
