c5f9304528296fd05b18bfd9ffd1ed2788a6dfc8
golden-dawn <constantin_m_adam@yahoo.com>
2019-06-28 03:04:49 -0400

    WIP: expiry analysis

--- stx_ana.h
+++ stx_ana.h
@@ -29,12 +29,12 @@ hashtable_ptr ana_data() {
     return stx;
 } 
 
-int ana_calc_spread(PGresult* sql_res, int spot) {
+void ana_option_analysis(ldr_ptr leader, PGresult* sql_res, int spot) {
     int itm_calls = 0, otm_calls = 0, itm_puts = 0, otm_puts = 0;
     int avg_spread = 0, bid, ask, strike, num_calls = 0, num_puts = 0;
     char* cp;
     bool call_atm = false, put_atm = false;
-    int num = PQntuples(sql_res), num_spreads = 0;
+    int num = PQntuples(sql_res), num_spreads = 0, atm_price = 0;
     for(int ix = 0; ix < num; ix++) {
 	cp = PQgetvalue(sql_res, ix, 0);
 	strike = atoi(PQgetvalue(sql_res, ix, 1));
@@ -51,12 +51,14 @@ int ana_calc_spread(PGresult* sql_res, int spot) {
 	}
     }
     if ((itm_calls < 2) || (otm_calls < 2) || (itm_puts < 2) || (otm_puts < 2))
-	return -1;
+	return;
     for(int ix = itm_calls - 2; ix < itm_calls; ix++) {
 	num_spreads++;
 	int bid = atoi(PQgetvalue(sql_res, ix, 2));
 	int ask = atoi(PQgetvalue(sql_res, ix, 3));
 	avg_spread += (100 - 100 * bid / ask);
+	if (ix == itm_calls - 1)
+	    atm_price += ask;
     }
     for(int ix = itm_calls; ix < itm_calls + (call_atm? 1: 2); ix++) {
 	num_spreads++;
@@ -69,6 +71,8 @@ int ana_calc_spread(PGresult* sql_res, int spot) {
 	int bid = atoi(PQgetvalue(sql_res, ix, 2));
 	int ask = atoi(PQgetvalue(sql_res, ix, 3));
 	avg_spread += (100 - 100 * bid / ask);
+	if (ix == num_calls + otm_puts - 1)
+	    atm_price += ask;
     }
     for(int ix = num_calls + otm_puts; 
 	ix < num_calls + otm_puts + (put_atm? 1: 2); ix++) {
@@ -77,14 +81,17 @@ int ana_calc_spread(PGresult* sql_res, int spot) {
 	int ask = atoi(PQgetvalue(sql_res, ix, 3));
 	avg_spread += (100 - 100 * bid / ask);
     }
-    return avg_spread / num_spreads;
+    leader->opt_spread = avg_spread / num_spreads;
+    if (leader->opt_spread >= 0)
+	leader->is_ldr = true;
+    leader->atm_price = atm_price / 2;
 }
 
 /** 
     This function returns the average option spread for stocks that are
     leaders, or -1, if the stock is not a leader. 
 **/
-ldr_ptr ana_leader(stx_data_ptr data, char* as_of_date) {
+ldr_ptr ana_leader(stx_data_ptr data, char* as_of_date, char* exp) {
     /** 
 	A stock is a leader at a given date if:
 	1. Its average activity is above a threshold.
@@ -110,14 +117,13 @@ ldr_ptr ana_leader(stx_data_ptr data, char* as_of_date) {
     if ((avg_act < MIN_ACT) || 
 	((1000 * avg_rg / data->data[data->pos].close) < MIN_RCR))
 	return leader;
-    char *exp, und[16];
+    char und[16];
     strcpy(und, data->stk);
     char* dot = strchr(und, '.');
     if(dot != NULL) {
 	if (( '0' <= *(dot + 1)) && (*(dot + 1)<= '9'))
             *dot = '\0';
     }
-    cal_expiry(cal_ix(as_of_date)+ 5, &exp);
     char sql_cmd[256];
     sprintf(sql_cmd, "select spot from opt_spots where stk='%s' and dt='%s'",
 	    und, as_of_date);
@@ -137,16 +143,13 @@ ldr_ptr ana_leader(stx_data_ptr data, char* as_of_date) {
 	PQclear(res);
 	return leader;
     }
-    int avg_spread = ana_calc_spread(res, spot);
+    ana_option_analysis(leader, res, spot);
     PQclear(res);
-    leader->opt_spread = avg_spread;
-    if (avg_spread >= 0)
-	leader->is_ldr = true;
     return leader;
 }
 
 
-void ana_expiry_analysis(char* dt) {
+int ana_expiry_analysis(char* dt) {
     /** 
      * special case when the date is an option expiry date
      * if the data is NULL, only run for the most recent business day
@@ -165,34 +168,46 @@ void ana_expiry_analysis(char* dt) {
     sprintf(sql_cmd, "%s%s%s", sql_1, dt, sql_2);
     PGresult *res = db_query(sql_cmd);
     int rows = PQntuples(res);
-    if (rows <= 0) {
-	LOGWARN("No stocks found for %s, exiting...\n", dt);
-	return;
+    LOGINFO("loaded %5d stocks\n", rows);
+    FILE* fp = NULL;
+    char *filename = "/tmp/leaders.csv", *exp;
+    cal_expiry(cal_ix(dt) + 5, &exp);
+    if((fp = fopen(filename, "w")) == NULL) {
+	LOGERROR("Failed to open file %s for writing\n", filename);
+	return -1;
     }
     for (int ix = 0; ix < rows; ix++) {
-	char stk_name[16];
-	strcpy(stk_name, PQgetvalue(res, ix, 0));
-	stx_data_ptr data = ht_get(ana_data(), stk_name);
-	if (data == NULL) {
-	    data = ts_load_stk(stk_name);
-
-	strcpy(all_stx[ix], PQgetvalue(res, ix, 0));
-    }
-    LOGINFO("stored %d stocks in list\n", rows);
-    PQclear(res);
-
-    for (int ix = 0; ix < rows; ix++) {
-	stx_data_ptr data = ts_load_stk(all_stx[ix]);
+	char stk[16];
+	strcpy(stk, PQgetvalue(res, ix, 0));
+	ht_item_ptr ht_data = ht_get(ana_data(), stk);
+	stx_data_ptr data = NULL;
+	if (ht_data == NULL) {
+	    data = ts_load_stk(stk);
+	    ht_data = ht_new_data(stk, data);
+	    ht_insert(ana_data(), ht_data);
+	} else
+	    data = ht_data->val.data;
+	ldr_ptr leader = ana_leader(data, dt, exp);
+	if (leader->is_ldr)
+	    fprintf(fp, "%s\t%s\t%d\t%d\t%d\t%d\n", exp, stk, leader->activity,
+		    leader->range_ratio, leader->opt_spread, 
+		    leader->atm_price);
+	free(leader);
 	if (ix % 100 == 0)
-	    LOGINFO("loaded %4d stocks\n", ix);
+	    LOGINFO("%s: analyzed %5d/%5d stocks\n", dt, ix, rows);
     }
-    LOGINFO("loaded all %4d stocks\n", rows);
+    fclose(fp);
+    LOGINFO("%s: analyzed %5d/%5d stocks\n", dt, rows, rows);
+    PQclear(res);
+    db_upload_file("leaders", filename);
+    LOGINFO("%s: uploaded leaders in the database as of date %s\n", exp, dt);
     /* 2. For each stock, get the data */
     /* 3. Set the time series to a specific date (dt) */
     /* 4. Get all the splits up to dt */
     /* 5. Adjust the data accordingly */
     /* 6. Run JL on the adjusted data */
     /* 7. Run the setups */
+    return 0;
 }
 
 
