ed7663afe7ecb488011e5a36e0b4823b5baf1765
golden-dawn <constantin_m_adam@yahoo.com>
2019-07-09 01:05:38 -0400

    Add -lcurl and -lcjson flags to debug compile

--- debug_compile.sh
+++ debug_compile.sh
@@ -2,4 +2,4 @@
 
 export SRC_FILE=$1
 export EXE_FILE="${SRC_FILE/\.c/\.exe}"
-gcc -DDEBUG -g -o ${EXE_FILE} ${SRC_FILE} -I. -I/usr/include/postgresql -lpq -lm
+gcc -DDEBUG -g -o ${EXE_FILE} ${SRC_FILE} -I. -I/usr/include/postgresql -lpq -lm -lcurl -lcjson
