bd82156ac4916590fecdd2077a7e396318b29fe2
golden-dawn <constantin_m_adam@yahoo.com>
2019-07-27 17:01:01 -0400

    Fixed backslash bug

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export SRC_FILE=$1
-export EXE_FILE="${SRC_FILE/\.c/\.exe}"
+export EXE_FILE="${SRC_FILE/\.c/.exe}"
 gcc -o ${EXE_FILE} ${SRC_FILE} -O3 -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl
