6fec121c39dcf1422f231d74907f0cd2b3f55dba
golden-dawn <constantin_m_adam@yahoo.com>
2019-07-28 02:43:27 -0400

    Fixed the backslash bug

--- debug_compile.sh
+++ debug_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export SRC_FILE=$1
-export EXE_FILE="${SRC_FILE/\.c/\.exe}"
+export EXE_FILE="${SRC_FILE/\.c/.exe}"
 gcc -DDEBUG -g -o ${EXE_FILE} ${SRC_FILE} -I. -I/usr/include/postgresql -lpq -lm -lcurl -lcjson
