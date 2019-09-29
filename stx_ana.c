73b5661221d739df4593eec4882bf0c9b311478f
golden-dawn <constantin_m_adam@yahoo.com>
2019-09-28 22:04:27 -0400

    WIP: add different analysis names, start/end dates

--- stx_ana.c
+++ stx_ana.c
@@ -31,16 +31,12 @@ int main(int argc, char** argv) {
 	    return 0;
 	}
     }
-    char *crs_date = "2002-02-15";
-    char *exp_date = "2002-02-16";
-    char *exp_bdate;
-    char *prev_exp_date = "2002-01-19";
-    char *end_date = (char *) calloc((size_t)16, sizeof(char));
-    char sql_cmd[128];
-/*     strcpy(sql_cmd, "explain analyze select * from eods"); */
-/*     PGresult *all_res = db_query(sql_cmd); */
-/*     PQclear(all_res); */
-/*     LOGINFO("got all records from eods\n"); */
+    char ana_name[64];
+    memset(ana_name, 64, 0);
+    strcpy(ana_name, "JC_Pullback");
+    char *crs_date = "2002-02-15",  *exp_date = "2002-02-16", *exp_bdate;
+    char *end_date = (char *) calloc((size_t)16, sizeof(char)), sql_cmd[128];
+
     strcpy(sql_cmd, "select max(dt) from eods");
     PGresult *res = db_query(sql_cmd);
     int num = PQntuples(res);
@@ -50,7 +46,17 @@ int main(int argc, char** argv) {
     }
     strcpy(end_date, PQgetvalue(res, 0, 0));
     PQclear(res);
-    printf("end_date = %s\n", end_date);
+
+    for (int ix = 0; ix < argc; ix++) {
+	if (!strcmp(argv[ix++], "--ana-name")) {
+	    memset(ana_name, 64, 0);
+	    strcpy(ana_name, argv[ix]);
+	} else if (!strcmp(argv[ix++], "--start-date"))
+	    strcpy(crs_date, argv[ix]);
+	else if (!strcmp(argv[ix++], "--end-date"))
+	    strcpy(end_date, argv[ix]);
+    }
+    LOGINFO("end_date = %s\n", end_date);
     int ix = cal_ix(crs_date), end_ix = cal_ix(end_date);
     int exp_ix = ix, exp_bix = cal_exp_bday(ix, &exp_bdate);
     cJSON *leaders = NULL;
@@ -65,23 +71,10 @@ int main(int argc, char** argv) {
 	    leaders = ana_get_leaders(exp_date, MAX_ATM_PRICE,
 				      MAX_OPT_SPREAD, 0);
 	}
-	/** TODO: pass the setup type as a parameter **/
-	ana_eod_analysis(crs_date, leaders, "JC_Pullback");
+	ana_eod_analysis(crs_date, leaders, ana_name);
 	ix = cal_next_bday(ix, &crs_date);
     }
     if (leaders != NULL)
 	cJSON_Delete(leaders);
-    /*
-      v 1. Implement cal_exp_bday() function
-      v 2. Retrieve max(dt) from eods.  This is the end_date
-      v 3. Get start_ix and end_ix indices.
-      v 4. while loop with stop condition start_ix <= end_ix
-      5. if !strcmp(start_date, exp_bdate
-             exp_ix = cal_expiry(ix + 1, &exp_date)
-             exp_bix = cal_exp_bday(exp_ix, &exp_bdate)
-	     ana_expiry()
-       6. ana_eod()
-       v 7. ix = cal_next_bday(ix, &start_date)
-     */
     free(end_date);
 }
