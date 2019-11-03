a285a98d57e8eafdfcb7dc0381f39fbfcd743291
golden-dawn <constantin_m_adam@yahoo.com>
2019-11-02 22:46:17 -0400

    Initial list of excluded stocks

---
+++ excluded.sql
@@ -0,0 +1,8 @@
+INSERT into excludes values ('DUST');
+INSERT into excludes values ('FAZ');
+INSERT into excludes values ('SOXS');
+INSERT into excludes values ('SDOW');
+INSERT into excludes values ('SQQQ');
+INSERT into excludes values ('SVXY');
+INSERT into excludes values ('UVXY');
+INSERT into excludes values ('VXX');
