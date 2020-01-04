f6795c5f90393217d90ad7c1b17022b55d913dd4
golden-dawn <constantin_m_adam@yahoo.com>
2020-01-04 03:02:37 -0500

    Print last l EOD lines

--- jl.c
+++ jl.c
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     strcpy(stk, "NFLX");
     strcpy(ed, "2019-05-17");
     float factor = 1.5;
-    int num_pivots = 4;
+    int num_pivots = 4, num_lines = 3;
     bool print_pivots_only = false, print_nils = false;
 
     for (int ix = 1; ix < argc; ix++) {
@@ -22,6 +22,8 @@ int main(int argc, char** argv) {
 	    factor = atof(argv[ix]);
 	else if (!strcmp(argv[ix], "-n") && (++ix < argc))
 	    num_pivots = atoi(argv[ix]);
+	else if (!strcmp(argv[ix], "-l") && (++ix < argc))
+	    num_lines = atoi(argv[ix]);
 	else if (!strcmp(argv[ix], "-ppo"))
 	    print_pivots_only = true;
 	else if (!strcmp(argv[ix], "-pnils"))
@@ -30,6 +32,10 @@ int main(int argc, char** argv) {
     stx_data_ptr data = ts_load_stk(stk);
     jl_data_ptr jl = jl_jl(data, ed, factor);
     jl_print(jl, print_pivots_only, print_nils);
+    for(int ix = jl->pos - num_lines; ix < jl->pos; ix++) {
+	ts_print_record(&(jl->data->data[ix]));
+	fprintf(stderr, "\n");
+    }
     int num;
     jl_print_pivots(jl, num_pivots, &num);
 }
