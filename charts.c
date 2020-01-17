bc1eb2b7449ba6096a0e903a63f502bc00597f31
golden-dawn <constantin_m_adam@yahoo.com>
2020-01-16 23:25:17 -0500

    Compilable version

--- charts.c
+++ charts.c
@@ -16,18 +16,18 @@ char* chart_xticks(stx_data_ptr data, int start, int end) {
     memset(xlabel, 0, 16);
     /** setup weekly labels */
     for(ix = start; ix <= end; ix++) {
-	if ((ix == 0) || (strcmp(strrchr(data[ix].date, '-'),
-				 strrchr(data[ix - 1].date, '-')))) {
-	    cursor = strrchr(data[ix].date, '-') + 1;
+	if ((ix == 0) || (strcmp(strrchr(data->data[ix].date, '-'),
+				 strrchr(data->data[ix - 1].date, '-')))) {
+	    cursor = strrchr(data->data[ix].date, '-') + 1;
 	    if (xlength > 64)
 		sprintf(xtics, "%s \"%s\" %d,", xtics, cursor, ix - start);
 	    else
 		change_month = 1;
 	}
-	else if ((ix == 0) || (strcmp(strchr(data[ix].date, '-'),
-				      strchr(data[ix - 1].date, '-')))) {
+	else if ((ix == 0) || (strcmp(strchr(data->data[ix].date, '-'),
+				      strchr(data->data[ix - 1].date, '-')))) {
 	    memset(xlabel, 0, 16);
-	    strcpy(xlabel, (strchr(data[ix].date, '-') + 1));
+	    strcpy(xlabel, (strchr(data->data[ix].date, '-') + 1));
 	    cursor = strrchr(xlabel, '-');
 	    *cursor = '\0';
 	    if(xlength > 64)
@@ -37,7 +37,7 @@ char* chart_xticks(stx_data_ptr data, int start, int end) {
 	}
 	else if ((xlength < 64) && (ix % 5 == 0)) {
 	    memset(xlabel, 0, 16);
-	    strcpy(xlabel, data[ix].date);
+	    strcpy(xlabel, data->data[ix].date);
 	    if (change_month == 0) {
 		cursor = strchr(xlabel, '-');
 		*cursor = '\0';
@@ -63,7 +63,7 @@ void draw_chart(char* name, char* filename, char* xtics, char* title,
     }
     fprintf(gp, "set grid x \n");
     fprintf(gp, "set grid y \n");
-    fprintf(gp, "set terminal png color \n");
+    fprintf(gp, "set terminal png \n");
     fprintf(gp, "set output \"/tmp/%s.png\" \n", name);
     fprintf(gp, "set multiplot \n");
     fprintf(gp, "set origin 0, 0 \n");
@@ -110,78 +110,38 @@ void chart_stock(char* name, char* start_date, char* end_date) {
 	if (min > data->data[ix].low)
 	    min = data->data[ix].low;
 	if (data->data[ix].open > data->data[ix].close)
-	    fprintf(fp, "%d ? ? ? ? %.2f %.2f %.2f %.2f %.0f\n", 
+	    fprintf(fp, "%d ? ? ? ? %d %d %d %d %d\n", 
 		    ix - start, data->data[ix].open, data->data[ix].high, 
 		    data->data[ix].low, data->data[ix].close, 
 		    data->data[ix].volume);
 	else
-	    fprintf(fp, "%d %.2f %.2f %.2f %.2f ? ? ? ? %.0f\n", 
+	    fprintf(fp, "%d %d %d %d %d ? ? ? ? %d\n", 
 		    ix - start, data->data[ix].open, data->data[ix].high, 
 		    data->data[ix].low, data->data[ix].close, 
 		    data->data[ix].volume);
     }
     fclose(fp);
-    xticks = set_xticks(data, start, end);
-    title = get_title(data, lines, end, name);
-    draw_chart(name, filename, xticks, title, end - start, min, max);
+    xticks = chart_xticks(data, start, end);
+    draw_chart(name, filename, xticks, name, end - start, min, max);
     free(xticks);
-    free(title);
     free( data);
     data= NULL;
 }
 
 
-void main( int argc, char* argv[]) {
-
-  name_value     parameters[ 128];
-  time_frame_ptr tf=    NULL;
-  char           *name= NULL, *value= NULL, *buffer= NULL; 
-  int            len, ix= 0, is_idx= 0;
-  char           filename[ 512], suffix[ 16];
-
-  memset( suffix, 0, 16);
-  printf( "Content-type: text/html\n\n");
-  printf( "<body bgcolor=#000000 text=#ffffff link=#ffffff vlink=#ffffff>");
-  buffer= strdup( getenv( "QUERY_STRING")); /** Assume GET */
-/*   printf( "buffer= %s<p>", buffer); */
-  name= buffer;
-  len= 0;
-  while( name!= NULL) {
-    value= strchr( name, '=');
-    if( value!= NULL) { 
-      *value++= '\0';
-      strcpy( parameters[ len].name, name);
-      name=  strchr( value, '&');
-      if( name!= NULL)
-	*name++='\0';
-      if( strcmp( value, ""))
-	strcpy( parameters[ len].value, value);
+int main(int argc, char* argv[]) {
+  int len, ix= 0, is_idx= 0;
+  char filename[512], start_date[16], end_date[16], stock[16];
+  for(int ix = 0; ix < argc; ix++) {
+      if (!strcmp(argv[ix], "-n") && (++ix < argc))
+	  strcpy(stock, argv[ix]);
+      else if (!strcmp(argv[ix], "-s") && (++ix < argc))
+	  strcpy(start_date, argv[ix]);
+      else if (!strcmp(argv[ix], "-e") && (++ix < argc))
+	  strcpy(end_date, argv[ix]);
       else
-	strcpy( parameters[ len].value, "nil");
-    } else {
-      name= NULL;
-    }
-    len++;
+	  LOGWARN("Unknown parameter %s\n", argv[ix]);
   }
-
-  if( !strcmp( parameters[ ix].name, "name")&&
-      strcmp( parameters[ ix].value, "nil"))
-    strcpy( filename, parameters[ ix].value);
-  else
-    is_idx= 1;
-  ix++;
-
-  if(( tf= parse_time_frame( len, parameters, &ix))!= NULL) {
-    if( !strcmp( parameters[ ix].name, "tf")&&
-	strcmp( parameters[ ix].value, "nil"))
-      strcpy( suffix, parameters[ ix].value);
-    if( is_idx== 1) {
-      chart_stock( "dji", *tf, suffix);
-      chart_stock( "nasdaq", *tf, suffix);
-      chart_stock( "sp500", *tf, suffix);
-    } else
-      build_chart_list( filename, *tf, suffix);
-    free( tf);
-  }
-  free( buffer);
+  chart_stock(stock, start_date, end_date);
+  return 0;
 }
