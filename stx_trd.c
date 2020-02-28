d6cb74a43e480f1d7f9b8551f21606d81f235aa8
golden-dawn <constantin_m_adam@yahoo.com>
2020-02-27 21:25:18 -0500

    Correct formatting

--- stx_trd.c
+++ stx_trd.c
@@ -23,7 +23,7 @@
       c. Iterate through the time series, and check the exit rules daily
       d. When an exit rule is triggered, 
           i. log the trade 
-	 ii. free the trade structure
+         ii. free the trade structure
 **/
 
 int main(int argc, char** argv) {
@@ -34,28 +34,28 @@ int main(int argc, char** argv) {
     PGresult *res = db_query(sql_cmd);
     int num = PQntuples(res);
     if (num < 1) {
-	LOGERROR("No data in the 'eods' table. Exiting!\n");
-	exit(-1);
+        LOGERROR("No data in the 'eods' table. Exiting!\n");
+        exit(-1);
     }
     strcpy(end_date, PQgetvalue(res, 0, 0));
     PQclear(res);
     for (int ix = 1; ix < argc; ix++) {
-	if (!strcmp(argv[ix], "--stocks") && (ix++ < argc - 1)) {
-	    stx = argv[ix];
-	    LOGINFO("stx = %s\n", stx);
-	} else if (!strcmp(argv[ix], "--setups") && (ix++ < argc - 1)) {
-	    setups = argv[ix];
-	    LOGINFO("setups = %s\n", setups);
-	} else if (!strcmp(argv[ix], "--start-date") && (ix++ < argc - 1)) {
- 	    start_date = argv[ix];
-	    LOGINFO("start_date = %s\n", start_date);
-	}
-	else if (!strcmp(argv[ix], "--end-date") && (ix++ < argc - 1)) {
-	    strcpy(end_date, argv[ix]);
-	    LOGINFO("end_date = %s\n", end_date);
-	}
-	else if (!strcmp(argv[ix], "--triggered-only"))
-	    triggered = true;
+        if (!strcmp(argv[ix], "--stocks") && (ix++ < argc - 1)) {
+            stx = argv[ix];
+            LOGINFO("stx = %s\n", stx);
+        } else if (!strcmp(argv[ix], "--setups") && (ix++ < argc - 1)) {
+            setups = argv[ix];
+            LOGINFO("setups = %s\n", setups);
+        } else if (!strcmp(argv[ix], "--start-date") && (ix++ < argc - 1)) {
+            start_date = argv[ix];
+            LOGINFO("start_date = %s\n", start_date);
+        }
+        else if (!strcmp(argv[ix], "--end-date") && (ix++ < argc - 1)) {
+            strcpy(end_date, argv[ix]);
+            LOGINFO("end_date = %s\n", end_date);
+        }
+        else if (!strcmp(argv[ix], "--triggered-only"))
+            triggered = true;
     }
     LOGINFO("start_date = %s\n", start_date);
     LOGINFO("end_date = %s\n", end_date);
