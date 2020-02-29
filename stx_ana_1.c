6acc7ef6e3882edc431e8d92778c20be23b7c749
golden-dawn <constantin_m_adam@yahoo.com>
2020-02-29 17:41:04 -0500

    WIP: main program for analysis

--- stx_ana_1.c
+++ stx_ana_1.c
@@ -1,3 +1,4 @@
+#include <cjson/cJSON.h>
 #include <libpq-fe.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -8,7 +9,56 @@
 
 
 int main(int argc, char** argv) {
-    bool rt_analysis = false;
+    bool rt_analysis = true, eod = true, expiry_only = false, hist_analysis = false;
+    char ana_name[32];
+    char *start_date = cal_current_busdate(5);
+    char *end_date = cal_current_busdate(5);
+    cJSON *stx = NULL;
+
+    for (int ix = 1; ix < argc; ix++) {
+        if (!strcmp(argv[ix], "--ana") && (ix++ < argc - 1)) {
+            strcpy(ana_name, argv[ix]);
+            rt_analysis = false;
+            LOGINFO("ana_name = %s\n", ana_name);
+        } else if (!strcmp(argv[ix], "--start-date") && (ix++ < argc - 1)) {
+            cal_move_bdays(argv[ix], 0, &start_date);
+            LOGINFO("start_date = %s\n", start_date);
+        } else if (!strcmp(argv[ix], "--end-date") && (ix++ < argc - 1)) {
+            cal_move_bdays(argv[ix], 0, &end_date);
+            LOGINFO("end_date = %s\n", end_date);
+        } else if (!strcmp(argv[ix], '--stx') && (ix++ < argc - 1)) {
+            stx = cJSON_CreateArray();
+            if (stx == NULL) {
+                LOGERROR("Failed to create stx cJSON Array.\n");
+                exit(-1);
+            }
+            cJSON *stk_name = NULL;
+            char* token = strtok(argv[ix], ",");
+            while (token) {
+                stk_name = cJSON_CreateString(token);
+                if (stk_name == NULL) {
+                    LOGERROR("Failed to create cJSON string for %s\n", token);
+                    continue;
+                }
+                cJSON_AddItemToArray(stx, stk_name);
+                token = strtok(NULL, ",");
+            }
+        } else if (!strcmp(argv[ix], "--expiry")) {
+            expiry_only = true;
+            /** TODO: how do we identify realtime expiry analysis? */
+            rt_analysis = true;
+        } else if (!strcmp(argv[ix], "--intraday")) {
+            eod = false;
+            rt_analysis = true;
+        }
+        else if (!strcmp(argv[ix], "--eod")) {
+
+        }
+
+    }
+
+
+
     if (argc == 2) {
         rt_analysis = true;
         char* crt_busdate = cal_current_busdate(5);
