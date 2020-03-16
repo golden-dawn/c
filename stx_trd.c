6e13ab5d4999726bc6cbe6ed3e9dedd875646aa9
golden-dawn <constantin_m_adam@yahoo.com>
2020-03-15 23:28:04 -0400

    WIP: add trade backtesting for scored setups

--- stx_trd.c
+++ stx_trd.c
@@ -27,9 +27,12 @@
 **/
 
 int main(int argc, char** argv) {
-    char *stx = "", *setups = "JC_1234,JC_5DAYS", *start_date = "2002-02-15";
+    char *stx = NULL, *setups = "scored", *start_date = "2002-02-15";
     char *end_date = (char *) calloc((size_t)16, sizeof(char)), sql_cmd[128];
     bool triggered = false;
+    char *tag = "JL";
+    int daily_num = 2, opt_spread = 12, min_score = 0;
+    
     strcpy(sql_cmd, "select max(dt) from eods");
     PGresult *res = db_query(sql_cmd);
     int num = PQntuples(res);
@@ -43,23 +46,38 @@ int main(int argc, char** argv) {
         if (!strcmp(argv[ix], "--stocks") && (ix++ < argc - 1)) {
             stx = argv[ix];
             LOGINFO("stx = %s\n", stx);
+        } else if (!strcmp(argv[ix], "--tag") && (ix++ < argc - 1)) {
+            tag = argv[ix];
+            LOGINFO("tag = %s\n", tag);
         } else if (!strcmp(argv[ix], "--setups") && (ix++ < argc - 1)) {
             setups = argv[ix];
             LOGINFO("setups = %s\n", setups);
         } else if (!strcmp(argv[ix], "--start-date") && (ix++ < argc - 1)) {
             start_date = argv[ix];
             LOGINFO("start_date = %s\n", start_date);
-        }
-        else if (!strcmp(argv[ix], "--end-date") && (ix++ < argc - 1)) {
+        } else if (!strcmp(argv[ix], "--end-date") && (ix++ < argc - 1)) {
             strcpy(end_date, argv[ix]);
             LOGINFO("end_date = %s\n", end_date);
-        }
-        else if (!strcmp(argv[ix], "--triggered-only"))
+        } else if (!strcmp(argv[ix], "--triggered-only"))
             triggered = true;
+        else if (!strcmp(argv[ix], "--daily-num") && (ix++ < argc - 1)) {
+             daily_num = atoi(argv[ix]);
+             LOGINFO("daily_num = %d\n", daily_num);
+        } else if (!strcmp(argv[ix], "--opt-spread") && (ix++ < argc - 1)) {
+             opt_spread = atoi(argv[ix]);
+             LOGINFO("opt_spread = %d\n", opt_spread);
+        } else if (!strcmp(argv[ix], "--min-score") && (ix++ < argc - 1)) {
+             min_score = atoi(argv[ix]);
+             LOGINFO("min_score = %d\n", min_score);
+        }
     }
+    LOGINFO("tag = %s\n", tag);
     LOGINFO("start_date = %s\n", start_date);
     LOGINFO("end_date = %s\n", end_date);
     LOGINFO("stx = %s\n", stx);
     LOGINFO("setups = %s\n", setups);
-    trd_trade(start_date, end_date, stx, setups, triggered);
+    if (!strcmp(setups, "scored"))
+        trd_trade_scored(tag, start_date, end_date, daily_num, opt_spread,
+                         min_score, stx);
+    trd_trade(tag, start_date, end_date, stx, setups, triggered);
 }
