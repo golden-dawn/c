8ce88581b3f78171b82813c680e6934a0f8d9ce9
golden-dawn <constantin_m_adam@yahoo.com>
2020-08-23 02:16:39 -0400

    Untabify

--- jl.c
+++ jl.c
@@ -15,46 +15,46 @@ int main(int argc, char** argv) {
     bool print_pivots_only = false, print_nils = false;
 
     for (int ix = 1; ix < argc; ix++) {
-	if (!strcmp(argv[ix], "-s") && (++ix < argc))
-	    strcpy(stk, argv[ix]);
-	else if (!strcmp(argv[ix], "-e") && (++ix < argc))
-	    strcpy(ed, argv[ix]);
-	else if (!strcmp(argv[ix], "-f") && (++ix < argc))
-	    factor = atof(argv[ix]);
-	else if (!strcmp(argv[ix], "-n") && (++ix < argc))
-	    num_pivots = atoi(argv[ix]);
-	else if (!strcmp(argv[ix], "-l") && (++ix < argc))
-	    num_lines = atoi(argv[ix]);
-	else if (!strcmp(argv[ix], "-ppo"))
-	    print_pivots_only = true;
-	else if (!strcmp(argv[ix], "-pnils"))
-	    print_nils = true;
+        if (!strcmp(argv[ix], "-s") && (++ix < argc))
+            strcpy(stk, argv[ix]);
+        else if (!strcmp(argv[ix], "-e") && (++ix < argc))
+            strcpy(ed, argv[ix]);
+        else if (!strcmp(argv[ix], "-f") && (++ix < argc))
+            factor = atof(argv[ix]);
+        else if (!strcmp(argv[ix], "-n") && (++ix < argc))
+            num_pivots = atoi(argv[ix]);
+        else if (!strcmp(argv[ix], "-l") && (++ix < argc))
+            num_lines = atoi(argv[ix]);
+        else if (!strcmp(argv[ix], "-ppo"))
+            print_pivots_only = true;
+        else if (!strcmp(argv[ix], "-pnils"))
+            print_nils = true;
     }
     stx_data_ptr data = ts_load_stk(stk);
     jl_data_ptr jl = jl_jl(data, ed, factor);
     jl_print(jl, print_pivots_only, print_nils);
     for(int ix = jl->pos - num_lines; ix < jl->pos; ix++) {
-	ts_print_record(&(jl->data->data[ix]));
-	fprintf(stderr, "\n");
+        ts_print_record(&(jl->data->data[ix]));
+        fprintf(stderr, "\n");
     }
     int num, ixx;
     jl_print_pivots(jl, num_pivots, &num);
 
     jl_pivot_ptr pivots = jl_get_pivots(jl, 4, &num);
     for(ixx = 0; ixx < num - 1; ixx++)
-	jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
-		     true, pivots[ixx].rg, pivots[ixx].obv);
+        jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
+                     true, pivots[ixx].rg, pivots[ixx].obv);
     jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
-		 false, pivots[ixx].rg, pivots[ixx].obv);
+                 false, pivots[ixx].rg, pivots[ixx].obv);
     free(pivots);
     pivots = NULL;
 
     pivots = jl_get_pivots_date(jl, "2018-01-01", &num);
     for(ixx = 0; ixx < num - 1; ixx++)
-	jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
-		     true, pivots[ixx].rg, pivots[ixx].obv);
+        jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
+                     true, pivots[ixx].rg, pivots[ixx].obv);
     jl_print_rec(pivots[ixx].date, pivots[ixx].state, pivots[ixx].price, 
-		 false, pivots[ixx].rg, pivots[ixx].obv);
+                 false, pivots[ixx].rg, pivots[ixx].obv);
     free(pivots);
     pivots = NULL;
 
