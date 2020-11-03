a9e83a0edbed153d8501e771e5349e90da046329
golden-dawn <constantin_m_adam@yahoo.com>
2020-11-02 23:58:19 -0500

    Reformat

--- stx_ana.h
+++ stx_ana.h
@@ -1480,8 +1480,8 @@ void ana_relative_strength(eq_value_ptr rs, char* dt, int num_stocks) {
             cJSON_AddNumberToObject(rs_info, "rs_rank", num_buckets - 1 - ix);
             char* rs_info_string = cJSON_Print(rs_info);
             char sql_cmd[1024];
-            sprintf(sql_cmd, "insert into indicators values ('%s', '%s', '%s') "
-                    "on conflict on constraint indicators_pkey do "
+            sprintf(sql_cmd, "insert into indicators values ('%s', '%s', '%s')"
+                    " on conflict on constraint indicators_pkey do "
                     "update set indicators='%s'", rs[ixx + processed].name, dt,
                     rs_info_string, rs_info_string);
             db_transaction(sql_cmd);
