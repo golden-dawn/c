4ce9c20f2f351c4957f17c7fc5008edf14c708b3
Golden Dawn <constantin_m_adam@yahoo.com>
2021-04-23 18:21:19 +0000

    Print compile command

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 
 export SRC_FILE=$1
 export EXE_FILE="${SRC_FILE/\.c/.exe}"
+echo -e "gcc -o ${EXE_FILE} ${SRC_FILE} -O3 -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl"
 gcc -o ${EXE_FILE} ${SRC_FILE} -O3 -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl
